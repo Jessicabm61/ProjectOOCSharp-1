b7821a1fe1a622cb44e8811ccbdf4174eac4a0c6
Jéssica <jessicabeatrizmonteiro1994@gmail.com>
2021-10-27 00:57:10 -0300

    Update Post.cs

--- Entities/Post.cs
+++ Entities/Post.cs
@@ -1,12 +1,53 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Exercício11
+namespace Course.Entities
 {
     class Post
     {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>(); //list object Comments 1 para muitos
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment) //Add um item a lista
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment) //Remove um item da lista
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString() //To string para imprimir a Classe
+        {
+            StringBuilder sb = new StringBuilder(); //Utilizando StringBuider para impressão da string
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments:");
+            foreach (Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
     }
 }
