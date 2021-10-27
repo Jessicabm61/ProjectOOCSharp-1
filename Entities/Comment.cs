cd75c01e1a4414c397ff0c424170387d5942471e
Jéssica <jessicabeatrizmonteiro1994@gmail.com>
2021-10-27 00:56:41 -0300

    Update Comment.cs

--- Entities/Comment.cs
+++ Entities/Comment.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exercício11
+namespace Course.Entities
 {
     class Comment
     {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
     }
 }
